0226759b686396a53bfe277b2d06d7567501c691
lagol <github@maxjacobi.com>
2022-03-03 10:45:03 +0100

    Added AeroFX

--- src/main/java/com/maxjacobi/genealogiesoftware/GenealogieSoftwareApplication.java
+++ src/main/java/com/maxjacobi/genealogiesoftware/GenealogieSoftwareApplication.java
@@ -6,13 +6,14 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class GenealogieSoftwareApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(GenealogieSoftwareApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
-        scene.getStylesheets().add(GenealogieSoftwareApplication.class.getResource("win7.css").toString());
+        scene.getStylesheets().add(Objects.requireNonNull(GenealogieSoftwareApplication.class.getResource("win7.css")).toString());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
--- src/main/java/com/maxjacobi/genealogiesoftware/HelloController.java
+++ src/main/java/com/maxjacobi/genealogiesoftware/HelloController.java
@@ -1,14 +1,7 @@
 package com.maxjacobi.genealogiesoftware;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
-    }
 }
